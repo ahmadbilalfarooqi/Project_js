dc59c03aac3f02c89f370b55b7fba21992308672
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2023-02-22 15:40:14 +0500

    add new file

---
+++ Loops/loopsObjects.js
@@ -0,0 +1,12 @@
+let car = {
+    make: 'Land Rover',
+    model: 'Defender 110',
+    year : '2023',
+    color : 'Black',
+
+}
+
+for (const key in car) {
+  console.log(car[key]);
+           
+}
