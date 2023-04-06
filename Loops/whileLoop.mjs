fa4c3370d6b300a62119050a630111a901c7ab5f
Ahmad Bilal Farooqi <ahmadbilal20152016@gmail.com>
2023-04-06 11:26:00 +0500

    update

--- Loops/whileLoop.mjs
+++ Loops/whileLoop.mjs
@@ -1,39 +1,38 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
-
-let userInput = prompt(`On a scale of 1-10, how much do you like the name Alexander?`)
+let userInput = prompt(
+  `On a scale of 1-10, how much do you like the name Alexander?`
+);
 userInput = Number(userInput);
 console.log(userInput);
 
-if (userInput > 0 && userInput < 11){
-    userInput = false
-} else{
-    userInput = true
-
+if (userInput > 0 && userInput < 11) {
+  userInput = false;
+} else {
+  userInput = true;
 }
 
 while (userInput) {
-    userInput = prompt(`On a scale of 1-10, how much do you like the name Alexander?`)
-    console.log(userInput);
+  userInput = prompt(
+    `On a scale of 1-10, how much do you like the name Alexander?`
+  );
+  console.log(userInput);
 }
 
-
-
-const maxValue = 10
-const randomNumber = Math.floor(Math.random()*maxValue) + 1
+const maxValue = 10;
+const randomNumber = Math.floor(Math.random() * maxValue) + 1;
 let status = false;
 
 while (!status) {
-    let userInput = prompt(`Enter a number between 1 and ${maxValue} `)
-    userInput = Number(userInput);
-    if (userInput === randomNumber){
-        status = true;
-        console.log(`You win! The number was ${randomNumber}`);
-    } else if (userInput > randomNumber){
-        console.log(`Sorry! your guess was too high. `);
-    } else{
-        console.log(`Sorry! your was too low. `);
-    }
+  let userInput = prompt(`Enter a number between 1 and ${maxValue} `);
+  userInput = Number(userInput);
+  if (userInput === randomNumber) {
+    status = true;
+    console.log(`You win! The number was ${randomNumber}`);
+  } else if (userInput > randomNumber) {
+    console.log(`Sorry! your guess was too high. `);
+  } else {
+    console.log(`Sorry! your was too low. `);
+  }
 }
-
--- concurrancy/passwordChecker.js
+++ concurrancy/passwordChecker.js
@@ -1,35 +1,33 @@
 const allowPasswords = ["Password", "Alexander", "PIAIC"];
 
-function checkPassword (password){
-    return allowPasswords.includes(password)
+function checkPassword(password) {
+  return allowPasswords.includes(password);
 }
 
-function login(passwrod){
-    return new Promise ((resolve,reject) => {
-        if (checkPassword(passwrod)) {
-            resolve({
-                status : true,
-            })
-        } else {
-            reject({
-                status : false,
-            })
-        }
-
+function login(passwrod) {
+  return new Promise((resolve, reject) => {
+    if (checkPassword(passwrod)) {
+      resolve({
+        status: true,
+      });
+    } else {
+      reject({
+        status: false,
+      });
+    }
+  });
+}
+function checker(password) {
+  login(password)
+    .then((value) => {
+      console.log("Authorize User");
+      console.log(value);
     })
-}
-function checker(password){
-    login(password)
-        .then(value => {
-            console.log("Authorize User");
-            console.log(value);
-        })
-        .catch(value => {
-            console.log("Reject User");
-            console.log(value);
-        })
-
+    .catch((value) => {
+      console.log("Reject User");
+      console.log(value);
+    });
 }
 
-checker("Alexander")
-checker("Imran")
+checker("Alexander");
+checker("Imran");
